0ed292c604558f45fadf5e470785e95876563d14
slychief <alexander.schindler@slychief.com>
2016-03-15 12:59:17 +0100

    #1 aded projects for jmir and rp_extract feature extractors
    Change-Id: Ia91e03010c5bdfbade6b0ccb273cdab20fd27af5

---
+++ mir-features-jmir/src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	
+	public static int printHelloWorld(String param) {
+		
+		
+		System.out.println("Hello World - Param:" + param);
+		
+		return 0;
+	}
+	
+}
---
+++ mir-features-rp_extract/src/test/java/MainTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void testMain(){
+		Main.printHelloWorld("Alex");
+	}
+}
